6e82c426b6c1f05c374b8a27405bfb8e02fa140d
Vitor Oliveira <vmfoliveira89@gmail.com>
2018-05-22 11:57:59 +0100

    Support iPhone X and enable zoom on webview preview

--- src/ios/DocumentWebviewViewController.m
+++ src/ios/DocumentWebviewViewController.m
@@ -14,26 +14,17 @@
 
 @implementation DocumentWebviewViewController
 
+@synthesize webView;
+@synthesize buttonCloseView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
     UINavigationBar *navbar = [[UINavigationBar alloc]initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, 80)];
-    //navbar.barTintColor = [UIColor whiteColor];
     navbar.barTintColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5];
     navbar.translucent = NO;
-   /* UIBarButtonItem *flipButton = [[UIBarButtonItem alloc]
-                                   initWithTitle:@"Flip"
-                                   style:UIBarButtonItemStylePlain
-                                   target:self
-                                   action:@selector(flipView)];
-    
-    navbar.topItem.leftBarButtonItem = flipButton; */
-    
     
     UIButton *backButton = [UIButton buttonWithType:UIButtonTypeContactAdd];
-   // backButton.frame = CGRectMake(0, 0, 40, 22); // custom frame
-   // [backButton setImage:[UIImage imageNamed:@"icon_chevron-left_red"] forState:UIControlStateNormal];
     [backButton addTarget:self action:@selector(backButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
     
     // set left barButtonItem with custom view
@@ -42,21 +33,32 @@
     //do something like background color, title, etc you self
     [self.view addSubview:navbar];
     
-    
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    [button addTarget:self
+    self.buttonCloseView = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.buttonCloseView addTarget:self
                action:@selector(backButtonPressed:)
      forControlEvents:UIControlEventTouchUpInside];
-    [button setTitle:@"Close View" forState:UIControlStateNormal];
-    button.frame = CGRectMake([[UIScreen mainScreen] bounds].size.width - 160.0, 10, 160.0, 40.0);
-    [self.view addSubview:button];
+    [self.buttonCloseView setTitle:@"Close View" forState:UIControlStateNormal];
     
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        self.buttonCloseView.frame = CGRectMake([[UIScreen mainScreen] bounds].size.width - 160.0, 30, 160.0, 40.0);
+    } else {
+        self.buttonCloseView.frame = CGRectMake([[UIScreen mainScreen] bounds].size.width - 160.0, 10, 160.0, 40.0);
+    }
+    [self.view addSubview:self.buttonCloseView];
     
-    
-    self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 50, [[UIScreen mainScreen] bounds].size.width, ([[UIScreen mainScreen] bounds].size.height - 50))];
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 70, [[UIScreen mainScreen] bounds].size.width, ([[UIScreen mainScreen] bounds].size.height - 70))];
+    } else {
+        self.webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 50, [[UIScreen mainScreen] bounds].size.width, ([[UIScreen mainScreen] bounds].size.height - 50))];
+    }
     [[self.webView scrollView] setContentOffset:CGPointMake(0,500) animated:YES];
     [self.webView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"window.scrollTo(0.0, 50.0)"]];
     
+    //Enabling zoom for preview
+    self.webView.scalesPageToFit=YES;
+    
+    //Handle with rotation on iPhoneX
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(rotated:) name:UIDeviceOrientationDidChangeNotification object:nil];
 }
 
 -(void) loadDocumentWithUrl:(NSString *) urlString {
@@ -73,6 +75,25 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)rotated:(NSNotification *)notification {
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+
+       if(orientation == UIInterfaceOrientationPortrait){
+            [self.webView setFrame:CGRectMake(0, 70, [[UIScreen mainScreen] bounds].size.width, ([[UIScreen mainScreen] bounds].size.height - 70))];
+            [self.buttonCloseView setFrame:CGRectMake([[UIScreen mainScreen] bounds].size.width - 160.0, 30, 160.0, 40.0)];
+        }
+        else if(orientation == UIInterfaceOrientationLandscapeLeft){
+            [self.webView setFrame:CGRectMake(0, 50, ([[UIScreen mainScreen] bounds].size.width - 35), ([[UIScreen mainScreen] bounds].size.height - 50))];
+            [self.buttonCloseView setFrame:CGRectMake([[UIScreen mainScreen] bounds].size.width - 160.0, 10, 160.0, 40.0)];
+        }
+        else if(orientation == UIInterfaceOrientationLandscapeRight) {
+            [self.webView setFrame:CGRectMake(35, 50, ([[UIScreen mainScreen] bounds].size.width - 35), ([[UIScreen mainScreen] bounds].size.height - 50))];
+            [self.buttonCloseView setFrame:CGRectMake([[UIScreen mainScreen] bounds].size.width - 160.0, 10, 160.0, 40.0)];
+        }
+    }
 }
 
 @end
