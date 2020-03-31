aadf8100b272b33f5ea0cea721022e14c61ec18c
André Grillo <andregrillo@gmail.com>
2020-03-31 16:13:45 +0100

    Converted from UIWebView to WKWebView

--- src/ios/DocumentWebviewViewController.h
+++ src/ios/DocumentWebviewViewController.h
@@ -3,16 +3,17 @@
 //  OutSystems
 //
 //  Created by Vitor Oliveira on 01/08/16.
-//
+//  Updated by Andre Grillo on 31/03/20
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
 @interface DocumentWebviewViewController : UIViewController
 {
-    UIWebView *webView;
+    WKWebView *webView;
 }
-@property (retain, nonatomic) UIWebView *webView;
+@property (retain, nonatomic) WKWebView *webView;
 @property (retain, nonatomic) UIButton *buttonCloseView;
 
 -(void) loadDocumentWithUrl:(NSString *) urlString;
